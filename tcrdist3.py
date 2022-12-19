e9e86363a45620ee0465842a96486447764e007a
Shweta Chavan <chavan.shweta@gmail.com>
2022-12-18 21:57:33 -0500

    Create tcrdist3.py

---
+++ tcrdist3.py
@@ -0,0 +1,10 @@
+ create env -name tcrdist3
+ 
+ pip install tcrdist3==0.2.2
+  
+## subpackages
+#palmotif for CDR3 logo generation
+#tcrsampler for generating V-J gene-matched background receptor sets
+  
+#pwseqdist for efficient and parallelizable pairwise distance computation
+  
