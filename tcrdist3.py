389365e84dbb4e44d751207ca5b4cc1b09b5005d
Shweta Chavan <chavan.shweta@gmail.com>
2022-12-18 22:12:31 -0500

    Update tcrdist3.py

--- tcrdist3.py
+++ tcrdist3.py
@@ -1,10 +1,17 @@
- create env -name tcrdist3
- 
- pip install tcrdist3==0.2.2
+## Prep
+# create env -name tcrdist3
+# pip install tcrdist3==0.2.2
+
+## Way to run
+# python3 tcrdist3.py
   
 ## subpackages
 #palmotif for CDR3 logo generation
-#tcrsampler for generating V-J gene-matched background receptor sets
-  
+#tcrsampler for generating V-J gene-matched background receptor sets (non productive from human/mouse)
 #pwseqdist for efficient and parallelizable pairwise distance computation
+
+from tcrsampler.setup_db import install_all_next_gen
+install_all_next_gen(dry_run = False)
+
+
   
